551ea5b0ddaade2539706468ae7363780d196acc
kshnchk <kruglovaksusa31@gmail.com>
2025-04-10 21:51:31 +0400

    Add files via upload

---
+++ задание 9/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Пользователь вводит строку и две подстроки: что заменить и на что.
+Программа заменяет все вхождения и выводит результат.
+*/
+
+namespace задание_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string s = Console.ReadLine();
+            Console.Write("Введите подстроку для замены: ");
+            string chS = Console.ReadLine();
+            Console.Write("Введите новую подстроку: ");
+            string newS = Console.ReadLine();
+            string res = s.Replace(chS, newS);
+            Console.WriteLine(res);
+        }
+    }
+}
